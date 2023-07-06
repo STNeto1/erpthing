69db1f951bb2fa636bd44b588c40ce2cd8179101
STNeto1 <gfreitasneto18@gmail.com>
2023-07-06 20:32:11 -0300

    initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,25 @@
+import { Match, Switch, type VoidComponent } from "solid-js";
+import { helloQuery } from "@rpc/queries";
+
+const Home: VoidComponent = () => {
+  const hello = helloQuery(() => ({
+    name: "from pRPC",
+  }));
+  return (
+    <>
+      <h1>ERP Thing</h1>
+
+      <Switch fallback={<div>Loading...</div>}>
+        <Match when={hello.isLoading}>
+          <div>Loading...</div>
+        </Match>
+
+        <Match when={hello.data}>
+          <p>{hello.data}</p>
+        </Match>
+      </Switch>
+    </>
+  );
+};
+
+export default Home;
