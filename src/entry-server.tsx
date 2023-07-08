b06b510b2442dd2ea3f5a6da5922157aa5d2ebb7
STNeto1 <gfreitasneto18@gmail.com>
2023-07-08 01:17:59 -0300

    middleware thing

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -7,7 +7,6 @@ import {
 
 import { auth } from "~/auth/lucia.server";
 
-const protectedRoutes = ["/"];
 const guestRoutes = ["/auth/sign-in", "/auth/sign-up"];
 
 export default createHandler(
@@ -22,11 +21,11 @@ export default createHandler(
         return redirect("/");
       }
 
-      if (!session && protectedRoutes.includes(pathname)) {
+      if (!session && !guestRoutes.includes(pathname)) {
         return redirect("/auth/sign-in");
       }
 
-      return forward(event); // if we got here, and the pathname is inside the `protectedPaths` array - a user is logged in
+      return forward(event);
     };
   },
   renderAsync((event) => <StartServer event={event} />),
